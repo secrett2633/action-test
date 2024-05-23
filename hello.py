d6d65591471ef33b9c3e404f58cd9ed5769d7839
HONGJAEHYEONG <secrett2633@kakao.com>
2024-05-23 04:53:58 +0000

    Add bubble sort

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
-print("Hello world")
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
 
+# 사용 예시
+arr = [64, 34, 25, 12, 22, 11, 90]
+sorted_arr = bubble_sort(arr)
+print("정렬된 배열:", sorted_arr)
